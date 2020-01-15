77edcfa8b3d90739923d1a299afbf4a22ef3f086
Roman Bolandeu <r.bolandau@gmail.com>
2020-01-15 16:52:00 +0300

    Создает ветку папок если их нет по всему пути

--- folders_from_list_py/main.py
+++ folders_from_list_py/main.py
@@ -6,7 +6,7 @@ try:
     for line in f.readlines():
         path = 'result/' + line.rstrip()
         try:            
-            os.mkdir(path)
+            os.mkdirs(path)
         except OSError:
             print ("Создать директорию %s не удалось" % path)
         else:
