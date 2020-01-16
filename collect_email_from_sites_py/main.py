ad499f347195998df1c06ee27a3eeaaab2de0c0b
Roman Bolandeu <r.bolandau@gmail.com>
2020-01-16 12:20:54 +0300

    добавил функцию сбора адресов

--- collect_email_from_sites_py/main.py
+++ collect_email_from_sites_py/main.py
@@ -29,6 +29,55 @@ def get_content(url, headers):
         return 'HTTP Error: {content}'.format(content=err.response.content)    
     # end get_content
 
+  
+def add_all_links_recursive(url, maxdepth=1):
+    # print('{:>5}'.format(len(links)), url[len(HOST):])
+    # извлекает все ссылки из указанного `url`
+    # и рекурсивно обрабатывает их
+    # глубина рекурсии не более `maxdepth`
+  
+    # список ссылок, от которых в конце мы рекурсивно запустимся
+    links_to_handle_recursive = []
+  
+    # получаем html код страницы
+    request = requests.get(url)
+    # парсим его с помощью BeautifulSoup
+    soup = BeautifulSoup(request.content, 'lxml')
+    # рассматриваем все теги <a>
+    for tag_a in soup.find_all('a'):
+        # получаем ссылку, соответствующую тегу
+        link = tag_a['href']
+        # если ссылка не начинается с одного из запрещённых префиксов
+        if all(not link.startswith(prefix) for prefix in FORBIDDEN_PREFIXES):
+            # проверяем, является ли ссылка относительной
+            # например, `/oplata` --- это относительная ссылка
+            # `http://101-rosa.ru/oplata` --- это абсолютная ссылка
+            if link.startswith('/') and not link.startswith('//'):
+                # преобразуем относительную ссылку в абсолютную
+                link = HOST + link
+            # проверяем, что ссылка ведёт на нужный домен
+            # и что мы ещё не обрабатывали такую ссылку
+            if urlparse(link).netloc == DOMAIN and link not in links:
+                links.add(link)
+                links_to_handle_recursive.append(link)
+  
+    if maxdepth > 0:
+        for link in links_to_handle_recursive:
+            add_all_links_recursive(link, maxdepth=maxdepth - 1)
+  
+def main():
+    add_all_links_recursive(HOST + '/')
+    for link in links:
+        print(link)
+  
+if __name__ == '__main__':
+    main()
+
+
+DOMAIN = '101-rosa.ru'
+HOST = 'http://' + DOMAIN
+FORBIDDEN_PREFIXES = ['#', 'tel:', 'mailto:']
+links = set()  # множество всех ссылок
 
 url = 'https://taisu-tb.ru/'
 	
