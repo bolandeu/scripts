f3feaf267b74cd78ff849d5a4da0736526d5b05e
Roman Bolandeu <r.bolandau@gmail.com>
2020-01-16 15:29:17 +0300

    Cобирает почту

--- collect_email_from_sites_py/main.py
+++ collect_email_from_sites_py/main.py
@@ -4,21 +4,20 @@ from bs4 import BeautifulSoup
 import lxml
 from urllib.parse import urlparse
 
-'''
-1. Переход на страницу контактов если нет на главной, контакты в разных вариациях
-2. Правильно собирать сами ящики
-'''
-
 def get_email(content):
     email_pattern = re.findall(r'[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+', content)
-    return set(email_pattern)
+    emails = set(email_pattern)
+    return emails
 # end get_email
 
-def get_content(url, headers):
+def get_content(url, headers, type='text'):
     try:   
-        r = requests.get(url, headers=headers, timeout=(1, 10))
-        if r.status_code == 200: # если страница существует 
-            return r.text # возвращаем содержимое страницы
+        request = requests.get(url, headers=headers, timeout=(1, 10))
+        if request.status_code == 200: # если страница существует 
+            if type == "content":
+                return request.content # возвращаем содержимое страницы
+            else:
+                return request.text # возвращаем содержимое страницы
     except requests.exceptions.ConnectTimeout:
         return 'Connection timeout'
     except requests.exceptions.ReadTimeout:
@@ -27,67 +26,58 @@ def get_content(url, headers):
         return 'Seems like dns lookup failed..'
     except requests.exceptions.HTTPError as err:
         return 'HTTP Error: {content}'.format(content=err.response.content)    
-    # end get_content
+# end get_content
 
-  
-def add_all_links_recursive(url, maxdepth=1):
-    # print('{:>5}'.format(len(links)), url[len(HOST):])
-    # извлекает все ссылки из указанного `url`
-    # и рекурсивно обрабатывает их
-    # глубина рекурсии не более `maxdepth`
-  
-    # список ссылок, от которых в конце мы рекурсивно запустимся
-    links_to_handle_recursive = []
-  
-    # получаем html код страницы
-    request = requests.get(url)
-    # парсим его с помощью BeautifulSoup
-    soup = BeautifulSoup(request.content, 'lxml')
-    # рассматриваем все теги <a>
-    for tag_a in soup.find_all('a'):
-        # получаем ссылку, соответствующую тегу
-        link = tag_a['href']
-        # если ссылка не начинается с одного из запрещённых префиксов
-        if all(not link.startswith(prefix) for prefix in FORBIDDEN_PREFIXES):
-            # проверяем, является ли ссылка относительной
-            # например, `/oplata` --- это относительная ссылка
-            # `http://101-rosa.ru/oplata` --- это абсолютная ссылка
-            if link.startswith('/') and not link.startswith('//'):
-                # преобразуем относительную ссылку в абсолютную
-                link = HOST + link
-            # проверяем, что ссылка ведёт на нужный домен
-            # и что мы ещё не обрабатывали такую ссылку
-            if urlparse(link).netloc == DOMAIN and link not in links:
-                links.add(link)
-                links_to_handle_recursive.append(link)
-  
-    if maxdepth > 0:
-        for link in links_to_handle_recursive:
-            add_all_links_recursive(link, maxdepth=maxdepth - 1)
-  
-def main():
-    add_all_links_recursive(HOST + '/')
-    for link in links:
-        print(link)
-  
-if __name__ == '__main__':
-    main()
+def get_pages_by_marker(content, markers):
+    links = set()  # множество ссылок    
+    soup = BeautifulSoup(content, 'lxml')
+    for tag in soup.find_all('a', href=True):
+        link = tag['href'] # выделяем ссылку            
+        #print(tag.text, link)
+        # проверяем на маркеры
+        for marker in markers:
+
+            if link.find(marker)>=0 or (tag.text).find(marker)>=0:
+                # если относительная делаем абсолютной
+                if link.find('http') == -1:
+                    link = url + link
+                # print(urlparse(url).netloc, urlparse(link).netloc)
+                # проверяем что ссылка внутренняя    
+                if urlparse(url).netloc == urlparse(link).netloc:
+                    links.add(link) #добавляем в множество
+    return links
+# end get_pages_by_marker
 
 
-DOMAIN = '101-rosa.ru'
-HOST = 'http://' + DOMAIN
-FORBIDDEN_PREFIXES = ['#', 'tel:', 'mailto:']
-links = set()  # множество всех ссылок
-
-url = 'https://taisu-tb.ru/'
-	
+# Заголовки для парсинга
 headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, ke Gecko) Chrome/61.0.3163.100 Safari/537.36'}
 
-content = get_content(url, headers)
+# Маркеры текста ссылок страницы контактов
+markers = ['Контакты', 'Контактная информация', 'Контакт', 'contacts', 'contact', 'kontakty']
 
-soup = BeautifulSoup(content,"lxml")
-print(soup)
+# Адрес
+url = 'http://onway-logistics.com'
+	
 
-#emails = get_email(content)
 
-# print(emails)
+def main():    
+    text = get_content(url, headers)
+    #print(text)
+    emails = get_email(text)
+    #print('Главная', emails)
+    if not emails:
+        content = get_content(url, headers, "content")
+        links = get_pages_by_marker(content, markers)
+        #print('Контакты', links)
+        for addr in links:
+            #print(addr)
+            text = get_content(addr, headers)            
+            emails_contact = get_email(text)
+            #print(emails_contact)
+            emails.update(emails_contact)
+    print(emails)
+# end main
+
+
+if __name__ == '__main__':
+    main()
